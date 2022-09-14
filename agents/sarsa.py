1898254c79a222ed0339f1df77f8d78467774e29
Rounak Patra <rounakpatra2000@gmail.com>
2022-09-14 21:58:42 +0530

    moved epsilon outside of SARSA class and added as parameter in pickAction

--- agents/sarsa.py
+++ agents/sarsa.py
@@ -4,7 +4,7 @@ class SARSA:
     """
     SARSA agent
     """
-    def __init__(self, num_states, num_actions, alpha, epsilon, discount_factor = 0.9):
+    def __init__(self, num_states, num_actions, alpha, discount_factor = 0.9):
         self.num_states = num_states
         self.num_actions = num_actions
         self.episode_count = 0
@@ -17,7 +17,6 @@ class SARSA:
 
         # constant alpha and epsilon
         self.alpha = alpha
-        self.epsilon = epsilon
 
         # initialize last seen state and action pair
         # used for on-policy updates
@@ -34,12 +33,12 @@ class SARSA:
 
 
 
-    def pickAction (self, state):
+    def pickAction (self, state, epsilon):
         """
         epsilon greedy choice of action from state
         """
         p = np.random.random()
-        if p < self.epsilon:
+        if p < epsilon:
             return int(np.random.choice(self.num_actions))
         else:
             return int(np.argmax(self.Q_values[state]))
